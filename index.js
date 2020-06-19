759ae36081e4fc23fe33c9a9c9ecf40d8d5d5c97
Ubuntu <ubuntu@ip-172-31-93-92.ec2.internal>
2020-06-19 13:22:15 +0000

    Added index.html and index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var lol=prompt("Please enter which 2d polygon you would like this awesome calculator to calculate.")
+if(lol==="trapezoid"){
+    var tr1=Number(prompt("Enter the top base."))
+    var tr2=Number(prompt("Enter the bottom base."))
+    var tr3=Number(prompt("Now enter the height."))
+    confirm((tr1+tr2)*(tr3)/2)
+}
